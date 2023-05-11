3d9137d603cf3bb647aee52403b050d4990ea73f
SkyNet91 <104870340+SkyNet91@users.noreply.github.com>
2023-05-11 09:22:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,42 +2,72 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const cloud = document.querySelector('.cloud');
 const textStart = document.querySelector('#text-start');
+
 const audioStart = new Audio('./audio/theme.mp3');
 const audioGameOver = new Audio('./audio/gameover.mp3');
+
+let isStart = false;
+
 const scoreElement = document.querySelector('#score');
 
 let score = 0;
 
-//Start do jogo//
-const start = () => {
-    textStart.style.color = "rgb(236, 236, 236)";
-    pipe.classList.add('pipe-animation');
-    cloud.classList.add('cloud-animation');
-
-    audioStart.play();
-}
+//Evento de teclado//
 document.addEventListener('keydown', (event) => {
-    if (event.code === "Space") {
+    
+    if(event.code === "Space" && isStart == true){
+        jump();
+        
+    }else if (event.code === "KeyS") {
         start();
     }
+
 });
 
-//evento de pulo//
-const jump = () => {
+//Start do jogo//
+function start (){
+    textStart.style.color = "#ececec";
+    pipe.classList.add('pipe-animation');
+    cloud.classList.add('cloud-animation');
+    
+    isStart=true;
+
+    playAudioStart();
+}
+
+function jump(){
     mario.classList.add('jump');
+
     setTimeout(() => {
         mario.classList.remove('jump')
     }, 500)
 }
-document.addEventListener('keydown', jump);
+
+function playAudioStart() {
+    audioStart.play();
+} 
+
+function playAudioGameOver() {
+    audioGameOver.play();
+} 
+
+function stopAudioStart() {
+    audioStart.pause();
+} 
+
+function stopAudio() {
+    audioGameOver.pause();
+}
 
 //evento de fim de jogo//
 const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const cloudPosition = cloud.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    let pipePosition = pipe.offsetLeft;
+    let cloudPosition = cloud.offsetLeft;
+    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
 
     if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
+
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
         cloud.style.animation = 'none';
@@ -52,15 +82,11 @@ const loop = setInterval(() => {
         textStart.style.color = "black";
         textStart.innerHTML = "<strong>GAME OVER</strong>";
 
-        function stopAudioStart() {
-            audioStart.pause();
-        } stopAudioStart();
+        stopAudioStart();
 
-        audioGameOver.play();
-
-        function stopAudio() {
-            audioGameOver.pause();
-        } setTimeout(stopAudio, 8000);
+        playAudioGameOver();
+        
+        setTimeout(stopAudio, 8000);
 
         clearInterval(loop);
 
@@ -68,8 +94,8 @@ const loop = setInterval(() => {
         setTimeout(function () {
             location.reload();
         }, 9000);
-    } 
-    else if (pipePosition <= -50) {
+        
+    }else if (pipePosition <= -50) {
         // O jogador passou pelo obstáculo com sucesso
         score++;
         scoreElement.textContent = score;
